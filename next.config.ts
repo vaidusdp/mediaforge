1736cc0b15fd20af7a9fad111b5dae5ee95dd696
vaidus2608m <vaidus2608m@gmail.com>
2026-07-14 11:04:07 +0530

    update experimental config key from middlewareClientMaxBodySize to proxyClientMaxBodySize

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    middlewareClientMaxBodySize: 70 * 1024 * 1024,
+    proxyClientMaxBodySize: 70 * 1024 * 1024,
   },
   reactCompiler: true,
 };
